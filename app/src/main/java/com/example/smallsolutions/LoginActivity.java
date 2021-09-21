247ebb10b9f5a5d209655195dd8fe572790bca56
Akash <akashvsh13@gmail.com>
2021-09-21 17:45:17 +0530

    Akash push

--- app/src/main/java/com/example/smallsolutions/LoginActivity.java
+++ app/src/main/java/com/example/smallsolutions/LoginActivity.java
@@ -3,8 +3,29 @@ package com.example.smallsolutions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    //Skip button
+    public void skip(View view){
+        Toast.makeText(getBaseContext(), "Under construction", Toast.LENGTH_SHORT).show();
+    }
+
+    //Forget password button
+    public void forgetPassword(View view){
+        Toast.makeText(getBaseContext(), "Under construction", Toast.LENGTH_SHORT).show();
+    }
+
+    //SignIn button
+    public void SignIn(View view){
+        Toast.makeText(getBaseContext(), "Under construction", Toast.LENGTH_SHORT).show();
+    }
+
+    //new user button
+    public void newUser(View view){
+        Toast.makeText(getBaseContext(), "Under construction", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
