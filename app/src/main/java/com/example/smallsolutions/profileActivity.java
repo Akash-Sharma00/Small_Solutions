801da2892ff3769b636e82d6c3b1a58a93fa1916
Akash <akashvsh13@gmail.com>
2021-09-28 19:07:30 +0530

    Profile Activity

--- app/src/main/java/com/example/smallsolutions/profileActivity.java
+++ app/src/main/java/com/example/smallsolutions/profileActivity.java
@@ -10,5 +10,6 @@ public class profileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getActionBar().setTitle("Profile");
     }
 }
