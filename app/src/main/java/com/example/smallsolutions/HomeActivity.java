01f48d625c937c089f51858e3c726a949b0b9d5a
Akash <akashvsh13@gmail.com>
2021-09-29 15:32:53 +0530

    Toolbar added

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     public void profile(View view){
         Intent intent = new Intent(this,profileActivity.class);
@@ -18,7 +22,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         TextView carpenter = findViewById(R.id.carpenter);
         carpenter.setBackgroundResource(R.drawable.blue);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar,menu);
+        return  true;
     }
 }
