5479ed061f80fbbad64cea0998983422e905d8e1
Akash <akashvsh13@gmail.com>
2021-09-29 15:58:45 +0530

    Toolbar added

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -1,13 +1,16 @@
 package com.example.smallsolutions;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
@@ -32,5 +35,23 @@ public class HomeActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar,menu);
         return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int choice = item.getItemId();
+        if (choice == R.id.search){
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+        }
+        if (choice == R.id.category){
+            Toast.makeText(this, "Under Construction", Toast.LENGTH_SHORT).show();
+        }
+        if (choice == R.id.settings){
+            Toast.makeText(this, "Under Construction", Toast.LENGTH_SHORT).show();
+        }
+        if (choice == R.id.login){
+            Toast.makeText(this, "Under Construction", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
