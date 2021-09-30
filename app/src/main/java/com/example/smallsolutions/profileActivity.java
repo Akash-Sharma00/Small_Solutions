64a74e3d5b3e404d1d9c0f0ac888eda4cb032711
Akash <akashvsh13@gmail.com>
2021-09-30 21:17:20 +0530

    Finishing touch

--- app/src/main/java/com/example/smallsolutions/profileActivity.java
+++ app/src/main/java/com/example/smallsolutions/profileActivity.java
@@ -10,6 +10,5 @@ public class profileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        getActionBar().setTitle("Profile");
     }
 }
