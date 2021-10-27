b2de125987752b803e33b2a177dcaa66a419809f
Akash <akashvsh13@gmail.com>
2021-10-27 18:29:56 +0530

    editing home a

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -2,9 +2,15 @@ package com.example.smallsolutions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+    public void catSearch(View view){
+        Intent intent = new Intent(this,Category.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
