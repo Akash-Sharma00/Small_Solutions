b92cb058307c287220b1147a3c759e3094ea1dc2
Akash <akashvsh13@gmail.com>
2021-10-27 16:37:20 +0530

    editing home a

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -1,23 +1,14 @@
 package com.example.smallsolutions;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
-    private Toolbar toolbar;
 
-    public void profile(View view){
-        Intent intent = new Intent(this,profileActivity.class);
-        startActivity(intent);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
     }
 }
