552400b6b731ad90f0bf9bfc2a19ffaa201c2caf
Akash <akashvsh13@gmail.com>
2021-10-27 19:01:46 +0530

    editing home q

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
     public void catSearch(View view){
@@ -16,5 +17,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView img = findViewById(R.id.imghome);
+        img.setColorFilter(R.drawable.blue);
     }
 }
