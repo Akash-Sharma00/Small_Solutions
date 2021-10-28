82268ff44854f63c371372a6e0872574531b3b65
Akash <akashvsh13@gmail.com>
2021-10-28 17:15:48 +0530

    editing nav

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -7,25 +7,75 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class HomeActivity extends AppCompatActivity {
 
-    public void catSearch(View view){
-        Intent intent = new Intent(this,Category.class);
-        startActivity(intent);
+    ImageView homeimg, searchimg, hiredimg, profileimg;
+    TextView hometxt, searchtxt, hiredtxt, profiletxt;
+
+    public void getElement(){
+        homeimg = findViewById(R.id.imghome);
+        searchimg = findViewById(R.id.imgsearch);
+        hiredimg = findViewById(R.id.imghired);
+        profileimg = findViewById(R.id.imgprofile);
+
+        hometxt = findViewById(R.id.hometxt);
+        searchtxt = findViewById(R.id.cattxt);
+        hiredtxt = findViewById(R.id.hiredtxt);
+        profiletxt = findViewById(R.id.profiletxt);
     }
 
-    public void profile (View view){
-        Intent intent = new Intent(this,profileActivity.class);
-        startActivity(intent);
+    public  void clearColor(){
+        getElement();
+        homeimg.clearColorFilter();
+        hometxt.setTextColor(R.drawable.search_bar_bg);
+
+        hiredimg.clearColorFilter();
+        hiredtxt.setTextColor(R.drawable.search_bar_bg);
+
+        profileimg.clearColorFilter();
+        profiletxt.setTextColor(R.drawable.search_bar_bg);
+
+        searchimg.clearColorFilter();
+        searchtxt.setTextColor(R.drawable.search_bar_bg);
+    }
+
+    public void homepage(View view){
+        getElement();
+        clearColor();
+        homeimg.setColorFilter(Color.BLUE);
+        hometxt.setTextColor(Color.BLUE);
+    }
+
+    public void catepage(View view){
+        getElement();
+        clearColor();
+        searchimg.setColorFilter(Color.BLUE);
+        searchtxt.setTextColor(Color.BLUE);
+    }
+
+    public void hiredpage(View view){
+        getElement();
+        clearColor();
+        hiredtxt.setTextColor(Color.BLUE);
+        hiredimg.setColorFilter(Color.BLUE);
+    }
+
+    public void profilepage(View view){
+        getElement();
+        clearColor();
+        profileimg.setColorFilter(Color.BLUE);
+        profiletxt.setTextColor(Color.BLUE);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
