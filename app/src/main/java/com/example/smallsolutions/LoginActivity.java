d5f489d28a90089b63e65d7c2282ced935d1b6b2
Nitin <nitinpoojary00@gmail.com>
2021-10-30 20:08:21 +0530

    Updated

--- app/src/main/java/com/example/smallsolutions/LoginActivity.java
+++ app/src/main/java/com/example/smallsolutions/LoginActivity.java
@@ -1,11 +1,18 @@
 package com.example.smallsolutions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -36,5 +43,74 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+//        Creating variable for viewpager, tablayout and adapter class
+        ViewPager2 pager2;
+        TabLayout tabLayout;
+        LoginFragmentAdapter adapter;
+
+//        Hooks
+        pager2 = findViewById(R.id.form_viewpager);
+        tabLayout = findViewById(R.id.form_tab);
+        adapter = new LoginFragmentAdapter(getSupportFragmentManager(), getLifecycle());
+
+//        Setting adapter for viewpager
+        pager2.setAdapter(adapter);
+
+//        Creating Tabs
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign up"));
+
+//        Hooking tablayout with viewpager
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+//        Collapse listener for appbar
+        AppBarLayout appBarLayout;
+
+        appBarLayout = findViewById(R.id.appbar_layout);
+
+//        function to listen collapsing of appbar
+        appBarLayout.addOnOffsetChangedListener((AppBarLayout.BaseOnOffsetChangedListener) (appBarLayout1, verticalOffset) -> {
+            if(Math.abs(verticalOffset) - appBarLayout1.getTotalScrollRange() == 0){
+                Toolbar toolbar;
+                toolbar = findViewById(R.id.collapsing_toolbar);
+                toolbar.setTitleTextColor(Color.rgb(0,0,0));
+
+                NestedScrollView scrollView;
+                scrollView = findViewById(R.id.form);
+                scrollView.setBackgroundColor(Color.rgb(255,255,255));
+            }
+            else{
+                Toolbar toolbar;
+                toolbar = findViewById(R.id.collapsing_toolbar);
+                toolbar.setTitleTextColor(Color.rgb(255,255,255));
+
+                NestedScrollView scrollView;
+                scrollView = findViewById(R.id.form);
+                scrollView.setBackgroundResource(R.drawable.form_background);
+            }
+        });
     }
 }
