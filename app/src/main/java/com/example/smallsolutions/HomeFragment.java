8af2ddcedea4dfb322ced53559285b1f1c7051a5
Akash <akashvsh13@gmail.com>
2021-11-10 18:35:48 +0530

    added drawer

--- app/src/main/java/com/example/smallsolutions/HomeFragment.java
+++ app/src/main/java/com/example/smallsolutions/HomeFragment.java
@@ -6,23 +6,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
-import android.widget.Toolbar;
-
-import com.google.android.material.navigation.NavigationView;
 
 
 public class HomeFragment extends Fragment implements OnClickListener{
 
     private DrawerLayout mDrawer;
-    private Toolbar toolbar;
-    private NavigationView nvDrawer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,8 +24,7 @@ public class HomeFragment extends Fragment implements OnClickListener{
         ImageView hamburger = myView.findViewById(R.id.burger);
         hamburger.setOnClickListener(this);
 
-        toolbar = myView.findViewById(R.id.toolbar);
-        mDrawer = myView.findViewById(R.id.drawer_layout);
+        mDrawer = getActivity().findViewById(R.id.drawer_layout);
 
 
         return myView;
@@ -41,8 +33,7 @@ public class HomeFragment extends Fragment implements OnClickListener{
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.burger){
-//            Toast.makeText(getActivity(),"hello", Toast.LENGTH_SHORT).show();
-             mDrawer.openDrawer(GravityCompat.START);
+            mDrawer.openDrawer(GravityCompat.START);
         }
     }
 
