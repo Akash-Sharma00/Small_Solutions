9160f19c248c196ef7ddbb619f4572ad08875c48
Akash <akashvsh13@gmail.com>
2021-11-10 16:50:03 +0530

    added drawer

--- app/src/main/java/com/example/smallsolutions/HomeFragment.java
+++ app/src/main/java/com/example/smallsolutions/HomeFragment.java
@@ -2,6 +2,8 @@ package com.example.smallsolutions;
 
 import android.os.Bundle;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,24 +14,27 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 
-public class HomeFragment extends Fragment implements OnClickListener {
+public class HomeFragment extends Fragment implements OnClickListener{
+    private DrawerLayout drawer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.fragment_home, container, false);
-        // Inflate the layout for this
         ImageView hamburger = myView.findViewById(R.id.burger);
         hamburger.setOnClickListener(this);
-        return myView;
+        drawer = myView.findViewById(R.id.drawer_layout);
 
+        return myView;
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.burger){
-            Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+            drawer.openDrawer(GravityCompat.START);
         }
-
     }
+
+
 }
