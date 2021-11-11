91368b4bef0576be2f7c660b7faa23425dc922c7
Akash <akashvsh13@gmail.com>
2021-11-11 10:18:28 +0530

    added drawer fir se

--- app/src/main/java/com/example/smallsolutions/HomeFragment.java
+++ app/src/main/java/com/example/smallsolutions/HomeFragment.java
@@ -1,21 +1,31 @@
 package com.example.smallsolutions;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 
-public class HomeFragment extends Fragment implements OnClickListener{
+public class HomeFragment extends Fragment implements OnClickListener, NavigationView.OnNavigationItemSelectedListener{
 
-    private DrawerLayout mDrawer;
+    DrawerLayout mDrawer;
+    Toolbar toolbar;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,9 +34,14 @@ public class HomeFragment extends Fragment implements OnClickListener{
         ImageView hamburger = myView.findViewById(R.id.burger);
         hamburger.setOnClickListener(this);
 
+        toolbar = myView.findViewById(R.id.toolbar);
         mDrawer = getActivity().findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawer, R.string.open_drawer, R.string.close_drawer);
 
-
+        mDrawer.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+        NavigationView navigationView = myView.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         return myView;
     }
 
@@ -38,4 +53,15 @@ public class HomeFragment extends Fragment implements OnClickListener{
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.first)
+        {
+            Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
 }
