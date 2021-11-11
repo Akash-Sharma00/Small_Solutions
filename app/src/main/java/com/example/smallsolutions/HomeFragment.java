f1803af5ac7c391e62c344ac0567b319f9f2c4e5
Akash <akashvsh13@gmail.com>
2021-11-11 17:35:20 +0530

    hash

--- app/src/main/java/com/example/smallsolutions/HomeFragment.java
+++ app/src/main/java/com/example/smallsolutions/HomeFragment.java
@@ -4,7 +4,9 @@ import android.app.ActionBar;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -21,47 +23,17 @@ import android.widget.Toolbar;
 import com.google.android.material.navigation.NavigationView;
 
 
-public class HomeFragment extends Fragment implements OnClickListener, NavigationView.OnNavigationItemSelectedListener{
-
-    DrawerLayout mDrawer;
-    Toolbar toolbar;
-    ActionBarDrawerToggle mDrawerToggle;
+public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         View myView = inflater.inflate(R.layout.fragment_home, container, false);
-        ImageView hamburger = myView.findViewById(R.id.burger);
-        hamburger.setOnClickListener(this);
 
-        toolbar = myView.findViewById(R.id.toolbar);
-        mDrawer = getActivity().findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawer, R.string.open_drawer, R.string.close_drawer);
-
-        mDrawer.addDrawerListener(mDrawerToggle);
-        mDrawerToggle.syncState();
-        NavigationView navigationView = myView.findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
         return myView;
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.burger){
-            mDrawer.openDrawer(GravityCompat.END);
-        }
-    }
-
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        int id = item.getItemId();
-        if (id == R.id.first)
-        {
-            Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
-        }
-
-        return false;
-    }
 }
