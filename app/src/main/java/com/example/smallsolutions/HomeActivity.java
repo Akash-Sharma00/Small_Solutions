1cab09ffdc81073a6be46fec91c41986be187126
Nitin <nitinpoojary00@gmail.com>
2021-12-19 12:16:11 +0530

    fixed a bug

--- app/src/main/java/com/example/smallsolutions/HomeActivity.java
+++ app/src/main/java/com/example/smallsolutions/HomeActivity.java
@@ -89,6 +89,7 @@ public class HomeActivity extends AppCompatActivity{
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        drawer.bringToFront();
     }
 
     @Override
