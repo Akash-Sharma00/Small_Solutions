54a9dbf414c179548c0e9ef63393deb51b46a1b0
Akash <akashvsh13@gmail.com>
2021-12-20 17:06:28 +0530

    handled back button

--- app/src/main/java/com/example/smallsolutions/AllRandom.java
+++ app/src/main/java/com/example/smallsolutions/AllRandom.java
@@ -2,6 +2,7 @@ package com.example.smallsolutions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AllRandom extends AppCompatActivity {
@@ -11,5 +12,12 @@ public class AllRandom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_random);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent home = new Intent(this,HomeActivity.class);
+        startActivity(home);
+        super.onBackPressed();
     }
 }
