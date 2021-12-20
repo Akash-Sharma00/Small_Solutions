d786d3a3cd8c7ea8e54e68615a071564f55c504b
Nitin <nitinpoojary00@gmail.com>
2021-12-20 20:06:16 +0530

    test run

--- app/src/main/java/com/example/smallsolutions/LoginActivity.java
+++ app/src/main/java/com/example/smallsolutions/LoginActivity.java
@@ -4,14 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -37,43 +32,43 @@ public class LoginActivity extends AppCompatActivity {
         welcome_text.animate().translationY(scrollView.getScrollY()).setDuration(0);
 
 //        Hooks
-//        pager2 = findViewById(R.id.form_viewpager);
-//        tabLayout = findViewById(R.id.form_tab);
-//        adapter = new LoginFragmentAdapter(getSupportFragmentManager(), getLifecycle());
-//
-////        Setting adapter for viewpager
-//        pager2.setAdapter(adapter);
-//
-////        Creating Tabs
-//        tabLayout.addTab(tabLayout.newTab().setText("Login"));
-//        tabLayout.addTab(tabLayout.newTab().setText("Sign up"));
+        pager2 = findViewById(R.id.form_viewpager);
+        tabLayout = findViewById(R.id.form_tab);
+        adapter = new LoginFragmentAdapter(getSupportFragmentManager(), getLifecycle());
+
+//        Setting adapter for viewpager
+        pager2.setAdapter(adapter);
+
+//        Creating Tabs
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign up"));
 
         tabLayout.setSelectedTabIndicatorColor(Color.rgb(48,99,184));
 
 //        Hooking tablayout with viewpager
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                pager2.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
-//
-//        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                tabLayout.selectTab(tabLayout.getTabAt(position));
-//            }
-//        });
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
 
 //        Collapse listener for collapsing toolbar
         AppBarLayout appBarLayout;
@@ -81,25 +76,25 @@ public class LoginActivity extends AppCompatActivity {
 
 //        function to listen collapsing of collapsing toolbar
 
-//        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-//            @Override
-//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-//                if(Math.abs(verticalOffset) - appBarLayout.getTotalScrollRange() == 0){
-//                    NestedScrollView scrollView;
-//                    scrollView = findViewById(R.id.form);
-//                    scrollView.setBackgroundColor(Color.rgb(255,255,255));
-//
-//                    CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
-//                    collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.white));
-//                    collapsingToolbarLayout.setContentScrimResource(R.drawable.toolbar_background);
-//                }
-//                else{
-//                    NestedScrollView scrollView;
-//                    scrollView = findViewById(R.id.form);
-//                    scrollView.setBackgroundResource(R.drawable.form_background);
-//                }
-//            }
-//        });
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if(Math.abs(verticalOffset) - appBarLayout.getTotalScrollRange() == 0){
+                    NestedScrollView scrollView;
+                    scrollView = findViewById(R.id.form);
+                    scrollView.setBackgroundColor(Color.rgb(255,255,255));
+
+                    CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+                    collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.white));
+                    collapsingToolbarLayout.setContentScrimResource(R.drawable.toolbar_background);
+                }
+                else{
+                    NestedScrollView scrollView;
+                    scrollView = findViewById(R.id.form);
+                    scrollView.setBackgroundResource(R.drawable.form_background);
+                }
+            }
+        });
 
 //        Funtion to create animation
         animation();
