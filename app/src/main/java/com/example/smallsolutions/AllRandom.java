cfedee47bac579e8ec7d514159a493fbc72ad66b
Akash <akashvsh13@gmail.com>
2021-12-22 20:11:26 +0530

    carrd added

--- app/src/main/java/com/example/smallsolutions/AllRandom.java
+++ app/src/main/java/com/example/smallsolutions/AllRandom.java
@@ -1,16 +1,33 @@
 package com.example.smallsolutions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllRandom extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<RecyclerGetterNSetter> cardList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_random);
+
+        cardList = new ArrayList<>();
+        cardList.add(new RecyclerGetterNSetter(R.drawable.mrplumber,4.4, "Akash", "Plumber"));
+        cardList.add(new RecyclerGetterNSetter(R.drawable.mrplumber,4.4, "Akash", "Plumber"));
+        cardList.add(new RecyclerGetterNSetter(R.drawable.mrplumber,4.4, "Akash", "Plumber"));
+
+
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerAdapter(cardList));
 
     }
 
