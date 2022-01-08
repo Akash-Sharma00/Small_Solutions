32e1c9cda44e17e050d0788531db716e6554d04b
Akash <akashvsh13@gmail.com>
2022-01-08 20:43:50 +0530

    database connected

--- app/src/main/java/com/example/smallsolutions/SignupFragment.java
+++ app/src/main/java/com/example/smallsolutions/SignupFragment.java
@@ -9,56 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class SignupFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignupFragment() {
-        // Required empty public constructor
-    }
-
-    // TODO: Rename and change types and number of parameters
-    public static SignupFragment newInstance(String param1, String param2) {
-        SignupFragment fragment = new SignupFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
-
-    AutoCompleteTextView autoCompleteTextView;    //for catagory
+    AutoCompleteTextView autoCompleteTextView;    //for category
 
     //    String arrays for drop down box
     String[] catagory = {"Job Seeker", "Recruiter"};
@@ -75,10 +39,16 @@ public class SignupFragment extends Fragment {
     ProfessionAdapter recyclerAdapter;
 
     //    Drop Down view for catagory
-    AutoCompleteTextView autoCompleteTextView_catagory;
+    AutoCompleteTextView autoCompleteTextView_category;
 
     //    Selected catagory String variable
     String catagory_string;
+
+//    Sign up button
+    Button signup;
+
+//All data in edittext
+    EditText name, mail, password, contact, age, exp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -88,6 +58,15 @@ public class SignupFragment extends Fragment {
 
         catatoryAdapter(root);
         professionAdapter(root);
+
+//        getting all edit text
+
+        name = root.findViewById(R.id.username_signup);
+        mail = root.findViewById(R.id.emailid_signup);
+        password = root.findViewById(R.id.password_signup);
+        contact = root.findViewById(R.id.contact_edittext);
+        age = root.findViewById(R.id.age);
+        autoCompleteTextView_category = root.findViewById(R.id.autoComplete_catagory);
 
         addText = root.findViewById(R.id.add_profession);
         removeText = root.findViewById(R.id.remove_profession);
@@ -106,12 +85,23 @@ public class SignupFragment extends Fragment {
             }
         });
 
+        signup = root.findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDataInFirebase();
+            }
+        });
+
         experienceAdapter(root);
 
-        autoCompleteTextView_catagory = root.findViewById(R.id.autoComplete_catagory);
+        autoCompleteTextView_category = root.findViewById(R.id.autoComplete_catagory);
         gone(root);
 
         return root;
+    }
+
+    private void addDataInFirebase() {
     }
 
     public void catatoryAdapter(View root) {
