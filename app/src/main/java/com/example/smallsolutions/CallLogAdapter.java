7f65ac1c11838d8fbf904fc43edd3fce1b6ab585
Akash <akashvsh13@gmail.com>
2022-02-17 16:36:11 +0530

    Permssion addded in call history

--- app/src/main/java/com/example/smallsolutions/CallLogAdapter.java
+++ app/src/main/java/com/example/smallsolutions/CallLogAdapter.java
@@ -1,20 +1,28 @@
 package com.example.smallsolutions;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.transition.Hold;
 import com.squareup.picasso.Picasso;
 
+import java.security.Permission;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -23,6 +31,7 @@ public class CallLogAdapter extends RecyclerView.Adapter<CallLogAdapter.viewHold
 
     Activity activity;
     ArrayList<UserDetails> Holder;
+    private final int REQUEST_CODE = 1;
 
     public CallLogAdapter(Activity activity, ArrayList<UserDetails> Holder) {
         this.Holder = Holder;
@@ -70,10 +79,18 @@ public class CallLogAdapter extends RecyclerView.Adapter<CallLogAdapter.viewHold
                 public void onClick(View v) {
                     String number = receiver_Contact.getText().toString();
                     String dial = "tel:" + number;
-                    itemView.getContext().startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                    if (ContextCompat.checkSelfPermission(
+                            v.getContext(),
+                            Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions((Activity)v.getContext() , new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE);
+                    }else {
+                        itemView.getContext().startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+                    }
+
                 }
             });
 
         }
+
     }
 }
